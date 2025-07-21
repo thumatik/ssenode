c0d4827961b0d77a666f096fcec160a48fa0768c
thumatik <71712466+thumatik@users.noreply.github.com>
2025-07-21 23:00:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,6 +24,8 @@ app.get('/events', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
   console.log('Server running on http://localhost:3000');
 });
